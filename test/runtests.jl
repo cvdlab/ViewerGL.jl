0af388cec7db03038f9ed8f56acf94a6e76f141b
apaoluzzi <apaoluzzi@mac.com>
2019-06-23 06:59:54 +0200

    added test/runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,20 @@
+using Test
+using LinearAlgebraicRepresentation
+Lar = LinearAlgebraicRepresentation
+
+include("./Box.jl")
+include("./Frustum.jl")
+include("./Geometry.jl")
+include("./GLColorBuffer.jl")
+include("./GLMesh.jl")
+include("./GLPhongShader.jl")
+include("./GLShader.jl")
+include("./GLText.jl")
+include("./GLUtils.jl")
+include("./GLVertexArray.jl")
+include("./GLVertexBuffer.jl")
+include("./Matrix.jl")
+include("./Point.jl")
+include("./Quaternion.jl")
+include("./Viewer.jl")
+include("./ViewerGL.jl")
