ec874719e0de5f703ace4efae3d22c401f58ad2f
apaoluzzi <apaoluzzi@mac.com>
2019-06-23 04:31:15 +0200

    renamed Mkpol.jl to Grid.jl

--- examples/hpc/Grid.jl
+++ examples/hpc/Grid.jl
@@ -6,13 +6,13 @@ GL = ViewerGL
 V,(VV,EV,FV,CV) = Lar.cuboidGrid([10,20,1],true)
 
 GL.VIEW([
-      GL.GLLar2gl(V,CV)
+      GL.GLGrid(V,FV,GL.Point4d(1,1,1,0.1))
 	GL.GLAxis(GL.Point3d(-1,-1,-1),GL.Point3d(1,1,1))
 ])
 
 GL.VIEW([
 	GL.GLGrid(V,VV,GL.COLORS[4])
 	GL.GLGrid(V,EV,GL.COLORS[2])
-	GL.GLGrid(V,FV,GL.COLORS[3])
+	GL.GLGrid(V,FV,GL.Point4d(0,1,0,0.1))
 	GL.GLAxis(GL.Point3d(-1,-1,-1),GL.Point3d(1,1,1))
 ])
