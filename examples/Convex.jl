eac9f63fd875a05bbdf3b4e79297640aac2f2fe5
apaoluzzi <apaoluzzi@mac.com>
2019-07-18 20:58:45 +0200

    inserted DataStructures in Project dependencies

--- examples/Convex.jl
+++ examples/Convex.jl
@@ -3,7 +3,7 @@ GL = ViewerGL
 
 points = rand(50,3)
 GL.VIEW([
-      GL.GLPoints(points)
-      GL.GLHull(points,GL.Point4d(1,1,1,0.2))
+      GL.GLPoints(points),
+      GL.GLHull(points,GL.Point4d(1,1,1,0.2)),
       GL.GLAxis(GL.Point3d(0,0,0),GL.Point3d(1,1,1))
 ]);
