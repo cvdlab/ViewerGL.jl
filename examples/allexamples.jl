bf04816e504ce2ff5dc12df8e55dd2cda094a0a0
apaoluzzi <apaoluzzi@mac.com>
2019-07-20 05:41:52 +0200

    posted new function `allexamples()`

---
+++ examples/allexamples.jl
@@ -0,0 +1,13 @@
+function allexamples()
+    include("Convex.jl")
+    include("Convex2d.jl")
+    include("Grid.jl")
+    include("Mesh.jl")
+    include("numbering.jl")
+    include("Points.jl")
+    include("Polygon.jl")
+    include("Polyhedron.jl")
+    include("Text.jl")
+end
+
+allexamples()
